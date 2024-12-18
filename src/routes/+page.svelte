aa08568bd41aae2b007b4bab68fa8b9af95477ca
Luke David Turnbull <lukedturnbull@gmail.com>
2024-12-18 21:07:57 +1100

    Removed styles for light mode, only need dark mode for this cosmic app!
    - Added shadcn svelte popover
    - Created calendar drop down
    - Created header with calendar in top right corner displaying todays date as the initial date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import Calendar from '$lib/components/ui/calendar/calendar.svelte';
+	import CalendarDropdown from '$lib/components/calendar-dropdown.svelte';
+	import { type DateValue, today, getLocalTimeZone } from '@internationalized/date';
 
-	let date = new Date();
+	let date = $state<DateValue>(today(getLocalTimeZone()));
 </script>
 
-<div>
-	<h1>Near Earth Objects</h1>
-	<div class="flex flex-col items-center justify-center">
-		<Calendar />
+<header class="fixed top-0 flex w-screen flex-row items-center justify-between px-4 py-2">
+	<div>
+		<h1>Near earth objects</h1>
 	</div>
-</div>
+	<div>
+		<CalendarDropdown bind:value={date} />
+	</div>
+</header>
+
+<main class="flex h-screen w-screen"></main>
